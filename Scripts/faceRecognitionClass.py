cad09c1e64edad30c6bc8109b04ab3eacb5382bb
Logan31and <loganbland18@yahoo.com>
2021-04-15 15:05:14 -0400

    Added file write for data processing
    Co-authored-by: Jalen Wayt <jmw0077@mail.wvu.edu>

--- Scripts/faceRecognitionClass.py
+++ Scripts/faceRecognitionClass.py
@@ -2,6 +2,10 @@ import numpy as np
 import cv2
 import os
 import json
+import threading
+import time
+import csv
+from datetime import datetime
 
 class FaceRecognizer:
     
@@ -16,24 +20,26 @@ class FaceRecognizer:
         
         # Assign the font when printing the user id to the screen
         self.font = cv2.FONT_HERSHEY_PLAIN
-     
-        # Get the Video from the Camera
-        self.videoCapture = cv2.VideoCapture(0)
-        
-        # Set the width of the window
-        self.videoCapture.set(3, 640)
-        
-        # Set the height of the window
-        self.videoCapture.set(4, 480)
-        self.minW = 0.1 * self.videoCapture.get(3)
-        self.minH = 0.1 * self.videoCapture.get(4)
+
+        # Assign the name
+        self.name = "Unknown"
     
     def recognize(self):
+        
+        # Get the Video from the Camera
+        videoCapture = cv2.VideoCapture(0)
+        # Set the width of the window
+        videoCapture.set(3, 640)
+        # Set the height of the window
+        videoCapture.set(4, 480)
+        minW = 0.1 * videoCapture.get(3)
+        minH = 0.1 * videoCapture.get(4)
+
         # Show the screens until closed
         while(True):
             
             # Reads each frame
-            ret, frame = self.videoCapture.read()
+            ret, frame = videoCapture.read()
             gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
             
             # Get the front facing faces in the frame
@@ -41,7 +47,7 @@ class FaceRecognizer:
                 gray,
                 scaleFactor=1.2,
                 minNeighbors=5,
-                minSize=(int(self.minW), int(self.minH))
+                minSize=(int(minW), int(minH))
             )
 
             # Get the side faces in the frame
@@ -49,7 +55,7 @@ class FaceRecognizer:
                 gray,
                 scaleFactor=1.2,
                 minNeighbors=5,
-                minSize=(int(self.minW), int(self.minH))
+                minSize=(int(minW), int(minH))
             )
             
             # Load the current users JSON file 
@@ -71,6 +77,9 @@ class FaceRecognizer:
                 self.determineIndividual(frame, front_faces, data)
             else:
                 print("No faces recognized")
+            
+            #Output the results to the csv file
+            self.writeResults()
                 
             #Set the title of the Window opened with the frame
             cv2.imshow('CS-472 Project', frame)
@@ -86,7 +95,7 @@ class FaceRecognizer:
                 break
 
         # Release the capture when the window is closed
-        self.videoCapture.release()
+        videoCapture.release()
         # Destroy all the windows created when the window is closed
         cv2.destroyAllWindows()
 
@@ -95,25 +104,41 @@ class FaceRecognizer:
     def determineIndividual(self, frame, faces, data):
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         for (x,y,w,h) in faces:
-            name, confidence = self.recognizer.predict(gray[y:y+h,x:x+w])
+            self.name, confidence = self.recognizer.predict(gray[y:y+h,x:x+w])
             # The less confidence, the more likely it is that inidivdual
             if(confidence < 100):
                 # Set the name if the data is not empty
                 if data is not None:
-                    name = data[str(name)]
+                    self.name = data[str(self.name)]
+                    cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
                 else:
-                    name = "Unknown"
+                    self.name = "Unknown"
+                    cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
                 confidence = " {0}%".format(round(100 - confidence))
-                cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
             else:
-                name = "Unknown"
+                self.name = "Unknown"
                 confidence = " {0}%".format(round(100 - confidence))
                 cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
 
-            cv2.putText(frame, str(name), (x+5, y-5), self.font, 1, (255, 255, 255), 2)
+            cv2.putText(frame, str(self.name), (x+5, y-5), self.font, 1, (255, 255, 255), 2)
             cv2.putText(frame, str(confidence), (x+5, y+h-5), self.font, 1, (255, 255, 0), 1)
                     
             roi_color = frame[y:y+h, x:x+w]
+
+    def writeResults(self):
+        if not os.path.isfile("../Results/results.csv"):
+            # Creates the file
+            #file = open('../Results/results.csv', '+a')
+            with open('../Results/results.csv', mode='w') as createFile:
+                create = csv.writer(createFile, delimiter=",", quotechar='"')
+                create.writerow(['Time', 'Name'])
+        
+        #if os.path.isfile("../Results/results.csv") and os.access("../Results/results.csv", os.R_OK):
+        with open('../Results/results.csv', mode='a') as resultsFile:
+            resultsWriter = csv.writer(resultsFile, delimiter=",",quotechar='"')
+            resultsWriter.writerow([datetime.now().strftime("%H:%M:%S"), self.name])
+
+
 
 def main():
     faceRecognizer = FaceRecognizer()
